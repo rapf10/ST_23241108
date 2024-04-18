8e0486d4550ce4650cb5427cab2734f6ac81e2e5
muo <71619031+rapf10@users.noreply.github.com>
2024-04-18 20:42:46 +0800

    Add files via upload

---
+++ Tipe Data.py
@@ -0,0 +1,44 @@
+#Tipe Data Integers, Float, String, Boolean
+a = 11
+z = -295
+d = 2.5
+c = "Ini Tipe Data"
+w = True
+
+print("String") 
+#String
+print( c, "\n")
+
+print("Integer")
+#Integer
+#Penjumlahan Bilangan
+Penjumlahan = a + z
+print("Penjumlahan a + z =", Penjumlahan)
+#Pengurangan Bilangan
+Pengurangan = a - z
+print("pengurangan a - z =", Pengurangan)
+#Perkalian Bilangan
+Perkalian = a * z
+print("Perkalian a * z =", Perkalian)
+#Pembagian Bilangan
+Pembagian = a / z
+print("Pembagian a / z =", Pembagian, "\n")
+
+print("Float")
+#Float
+#Penjumlahan Bilangan
+Penjumlahan = d + z
+print("Penjumlahan d + z =", Penjumlahan)
+#Pengurangan Bilangan
+Pengurangan = d - z
+print("pengurangan d - z =", Pengurangan)
+#Perkalian Bilangan
+Perkalian = d * z
+print("Perkalian d * z =", Perkalian)
+#Pembagian Bilangan
+Pembagian = d / z
+print("Pembagian d / z =", Pembagian, "\n")
+
+print("Boolean")
+print(a < d)
+print(a > d)
