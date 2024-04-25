9efac2f56db05c01ff76e711d53201ae616c7794
muo <71619031+rapf10@users.noreply.github.com>
2024-04-25 13:38:43 +0800

    Add files via upload

---
+++ meet3.py
@@ -0,0 +1,16 @@
+x = 3 # variabel x bernilai 3
+
+# Tipe Data
+
+#Tipe Data Integer
+bil_integer = 9
+print ("Nilai bil_integer =", bil_integer , "jenis Tipe Datanya =", type(bil_integer))
+
+#Tipe Data Float
+bil_float = 3.5
+print ("Nilai bil_float =", bil_float , "jenis Tipe Datanya =", type(bil_float))
+
+
+#Tipe Data String (kumpulan karakter)
+bil_string = " my name is rezka"
+print ("Nilai bil_string =", bil_string , "jenis Tipe Datanya =", type(bil_string))
