c6bf63159e7a93c07fd1437716929993450639fd
muo <71619031+rapf10@users.noreply.github.com>
2024-05-16 13:51:55 +0800

    Add files via upload

---
+++ meet6.py
@@ -0,0 +1,43 @@
+# perbandingan
+# lebh dari >
+# kurang dari <
+# lebih dari sama dengan >=
+# kurang dari sama dengan <=
+# sama dengan ==
+# tidak sama dengan!=
+# is
+# is not
+
+
+a = 3
+b = 6
+# lebih dari >
+hasil = a > b
+print (a, ">", b, "=", hasil)
+#kurang dari <
+hasil = a < b
+print (a, "<", b, "=", hasil)
+
+# lebih dari sama dengan >=
+hasil = a >= 2
+print (a, ">=", 2, "=", hasil)
+
+# kurang dari sama dengan
+hasil = a <= 4
+print (a, "<=", 4, "=", hasil)
+
+# sama dengan ==
+hasil = a == 3
+print (a, "==", 3, "=", hasil)
+
+# tidak sama dengan !=
+hasil = a != b
+print (a, "!=", b, "=", hasil)
+
+# is
+hasil = a is 3
+print (a, "is", 3, "=", hasil)
+
+# is not
+hasil = a is not b
+print (a, "is not", b, "=", hasil)
