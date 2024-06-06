4da89b257a3e09eec3137a088c87fe07f8f4e770
muo <71619031+rapf10@users.noreply.github.com>
2024-06-06 14:15:54 +0800

    Add files via upload

---
+++ meet8.py
@@ -0,0 +1,20 @@
+# Percabangan
+
+nama = input("silahkan masukkan nama : ")
+# IF kondisi : aksi
+# if nama == "edy" : print ("Dosen ganteng")
+# print("nama yang dimasukkan tidak ganteng")
+
+
+#if nama == "edy" :
+ #   print("Dosen ganteng")
+  #  print("Ganteng Banget")
+   # print(f"Ganteng Banget {nama}")
+#print("nama yang dimasukkan tidak ganteng")
+    
+    
+if nama == "edy" :
+    print("Dosen ganteng")
+    print("Ganteng Banget")
+else :
+    print("nama yang dimasukkan salah")
