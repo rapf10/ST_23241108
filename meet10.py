c0b0079389327d4f9cd991cfbb830931adbf73b2
muo <71619031+rapf10@users.noreply.github.com>
2024-06-21 22:16:41 +0800

    Add files via upload

---
+++ meet10.py
@@ -0,0 +1,36 @@
+#latihan perulangan
+#bil = 1
+#print (bil)
+#bil = 2
+#print (bil)
+#bil = 3
+#print (bil)
+#bil = 4
+#print (bil)
+#bil = 5
+#print (bil)
+
+#if kondisi
+#aksi
+
+bil = [1,2,3,4,5] #ini menggunakan list
+print (bil)
+
+for i in bil :
+    print (f"nilai i sekarang : {i}")
+print ("selesai 1")
+
+bil2 = range(5) #ini menggunakan range
+for i in bil2 :
+    print (f"nilai i sekarang : {i}")
+print ("selesai 2")
+
+bil3 = range(2,5) #ini menggunakan range
+for i in bil3 :
+    print ("kkday manis")
+    print (f"nilai i sekarang : {i}")
+print ("selesai 3")
+
+
+
+
